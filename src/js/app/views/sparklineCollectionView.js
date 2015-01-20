fc27949cfe6d5b7de9ebf8544b8b91f337f80d36
Matt Hampel <matth@localdata.com>
2015-01-20 18:37:21 -0500

    Add missing files

---
+++ src/js/app/views/sparklineCollectionView.js
@@ -0,0 +1,26 @@
+/*jslint nomen: true */
+/*globals define: true */
+
+define(function(require, exports, module) {
+  'use strict';
+
+  // Libs
+  var $ = require('jquery');
+  var _ = require('underscore');
+  var Backbone = require('backbone');
+  var L = require('leaflet');
+  var Marionette = require('marionette');
+
+  // App
+  var settings = require('app/settings');
+  var SparklineView = require('app/views/sparklineView');
+  var template = require('text!templates/sparklineContainer.html');
+
+  var SparklineCollectionView = Backbone.Marionette.CompositeView.extend({
+    template: _.template(template),
+    className: 'sparklines',
+    childView: SparklineView
+  });
+
+  return SparklineCollectionView;
+});
---
+++ src/js/app/views/toolsView.js
@@ -0,0 +1,28 @@
+/*jslint nomen: true */
+/*globals define: true */
+
+define(function(require, exports, module) {
+  'use strict';
+
+  // Libs
+  var $ = require('jquery');
+  var _ = require('underscore');
+  var Backbone = require('backbone');
+  var Chartist = require('chartist');
+  var L = require('leaflet');
+  var Marionette = require('marionette');
+
+  // App
+  var settings = require('app/settings');
+
+  // Templates
+  var template = require('text!templates/tools.html');
+
+  var OverviewView = Marionette.ItemView.extend({
+    template: _.template(template),
+
+    className: 'tools'
+  });
+
+  return OverviewView;
+});
